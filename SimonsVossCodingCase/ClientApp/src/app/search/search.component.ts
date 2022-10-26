077fa5cc42a59bade2779943859cc3a81730cfd1
Yurii Odrzhekhovskyi <yuraodr@gmail.com>
2022-10-26 23:44:41 +0200

    added responsive view for mobiles

--- SimonsVossCodingCase/ClientApp/src/app/search/search.component.ts
+++ SimonsVossCodingCase/ClientApp/src/app/search/search.component.ts
@@ -5,6 +5,7 @@ import { SearchService } from '../services/search.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
   public results: SearchResult[] = [];
